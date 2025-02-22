7d4d64aef100af12f4065dde70c30757fcc99de0
K. Banerjee <banerkx_2000@yahoo.com>
2025-02-21 18:48:02 -0700

    [master] [host: quantum] applied shellcheck fixes

--- test/fixtures/temp/test_helper.bash
+++ test/fixtures/temp/test_helper.bash
@@ -1,3 +1,7 @@
+# shellcheck shell=bash
+
+# NOTE: BATS_TEST_DIRNAME is defined elsewhere.
+# shellcheck disable=SC2154
 export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/../../.."
 export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/..}"
 
